d6c2ae32518500d61f8ae680dd4693ac961f0750
moonldfirefly <75151283+moonldfirefly@users.noreply.github.com>
2021-01-06 22:49:33 +0700

    FirstCommit

---
+++ Origin/Origin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Origin
+//
+//  Created by Moonlanding Panpan on 6/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
